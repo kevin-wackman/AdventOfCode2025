789d6bccf3dee9eaf8b3a0fc479ae8e917d15b1d
kevin-wackman <kevin.wackman@gmail.com>
2025-12-02 15:59:31 -0600

    Add solution to part 2 of day 1
      This tries to keep the same logic in the same function, so the
      original move function now also counts zeroes and part 1 throws that
      part away.  I had zero as a constant in part 1, but the logic is
      specific enough in part 2 that it made sense to treat 0 as part of
      the logic itself.

--- src/days/day1.py
+++ src/days/day1.py
@@ -14,12 +14,18 @@ def main():
     lines = list(map(str.strip, open_data_file_as_lines(DATA_FILE)))
     pointers = get_dial_pointers(lines, DIAL_SIZE, DIAL_START_POSITION)
     print(pointers.count(0))
+    total_zero_passes = get_times_dial_passes_zero(lines, DIAL_SIZE, DIAL_START_POSITION)
+    print(total_zero_passes)
 
-def make_move(instruction: str, dial_size: int, position: int) -> int:
+def make_move_count_zeroes(instruction: str, dial_size: int, position: int) -> tuple[int,int]:
     (direction, magnitude) = read_instruction(instruction)
     new_position = (direction.value * magnitude) + position
+    passes = 0
+    if (position != 0) and new_position <= 0:
+        passes += 1
     real_position = new_position % dial_size
-    return real_position
+    passes += (abs(new_position) // dial_size)
+    return (real_position, passes)
 
 def read_instruction(instruction: str) -> tuple[Direction, int]:
     if not instruction:
@@ -36,8 +42,17 @@ def get_dial_pointers(instructions: list[str], dial_size: int, start_position: i
     pointers = [start_position]
     pos = start_position
     for instruction in instructions:
-        pos = make_move(instruction, dial_size, pos)
+        (pos, _) = make_move_count_zeroes(instruction, dial_size, pos)
         pointers.append(pos)
     return pointers
+
+def get_times_dial_passes_zero(instructions: list[str], dial_size: int, start_position: int) -> int:
+    pos = start_position
+    total_passes = 0
+    for instruction in instructions:
+        (pos, passes) = make_move_count_zeroes(instruction, dial_size, pos)
+        total_passes += passes
+    return total_passes
+
 
 main()
