a89091a8c13f00f6f8e4f1b853b2977d258feb2a
kevin-wackman <kevin.wackman@gmail.com>
2025-12-08 15:10:08 -0600

    Add solution to part 2 of day 7 and genericize the grid library

--- src/days/day7.py
+++ src/days/day7.py
@@ -9,24 +9,37 @@ SPLITTER = '^'
 BEAM = '|'
 
 
-
-
-def cascade_beams(grid: Grid) -> int:
+def cascade_beams(grid: Grid) -> tuple[int, int]:
     splits = 0
-    grid[0] = [BEAM if x == START else x for x in grid[0]]
+    grid[0] = [1 if x == START else x for x in grid[0]]
     for (row, line) in enumerate(grid):
         if row == 0:
             pass
         else:
             for (col, char) in enumerate(line):
-                if safe_grid_get(grid, row-1, col) == BEAM:
+                above = safe_grid_get(grid, row-1, col)
+                if isinstance(above, int):
                     if char == EMPTY_SPACE:
-                        safe_grid_set(grid, row, col, BEAM)
+                        safe_grid_set(grid, row, col, above)
+                    elif isinstance(char, int):
+                        safe_grid_set(grid, row, col, char+above)
                     elif char == SPLITTER:
-                        safe_grid_set(grid, row, col-1, BEAM)
-                        safe_grid_set(grid, row, col+1, BEAM)
+                        left  = safe_grid_get(grid, row, col-1)
+                        if isinstance(left, int):
+                            safe_grid_set(grid, row, col-1, above+left)
+                        else:
+                            safe_grid_set(grid, row, col-1, above)
+                        right = safe_grid_get(grid, row, col+1)
+                        if isinstance(right, int):
+                            safe_grid_set(grid, row, col+1, above+right)
+                        else:
+                            safe_grid_set(grid, row, col+1, above)
                         splits += 1
-    return splits
+    paths = 0
+    for n in grid[-1]:
+        if isinstance(n, int):
+            paths += n
+    return (splits, paths)
 
 def main():
     lines = list(map(str.strip, open_data_file_as_lines(DATA_FILE)))
--- src/util/grid.py
+++ src/util/grid.py
@@ -1,11 +1,15 @@
-Grid = list[list[str]]
+from typing import TypeVar, List, Optional
 
-def safe_grid_get(grid: Grid, row: int, col: int, default="") -> str:
+T = TypeVar('T')
+
+Grid = List[List[T]]
+
+def safe_grid_get(grid: Grid[T], row: int, col: int) -> Optional[T]:
     if (0 <= row < len(grid)) and (0 <= col < len(grid[row])):
         return grid[row][col] 
-    return default
+    return None
 
-def safe_grid_set(grid: Grid, row: int, col: int, val: str) -> bool:
+def safe_grid_set(grid: Grid[T], row: int, col: int, val: T) -> bool:
     if (0 <= row < len(grid)) and (0 <= col < len(grid[row])):
         grid[row][col] = val
         return True
