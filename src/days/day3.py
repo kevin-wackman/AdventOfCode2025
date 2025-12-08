ff44f5b5f20f91236927fe0639d0f780a325c2da
kevin-wackman <kevin.wackman@gmail.com>
2025-12-08 08:39:35 -0600

    Add solution to part 2 of day 3
      This was super straightforward, and I could even replace my first
      solution with it since it is incidentally solved.

--- src/days/day3.py
+++ src/days/day3.py
@@ -2,42 +2,32 @@ from util.filehandling import open_data_file_as_lines
 
 DATA_FILE = "day3in.txt"
 
+PART_1_LENGTH = 2
+PART_2_LENGTH = 12
+
 
 def main():
     lines = list(map(str.strip, open_data_file_as_lines(DATA_FILE)))
-    sum = 0
+    part_1_sum = 0
+    part_2_sum = 0
     for line in lines:
-        sum += get_joltage(line)
-    print(sum)
-
-def get_joltage(bank: str) -> int:
-    bank_list = list(map(int, bank))
-    largest_number = bank_list[0]
-    largest_after_number = 0
-    for n in bank_list[1:-1]:
-        if n > largest_number:
-            largest_number = n
-            largest_after_number = 0
-        elif n > largest_after_number:
-            largest_after_number = n
-    if bank_list[-1] > largest_after_number:
-        largest_after_number = bank_list[-1]
-    return (10*largest_number) + largest_after_number
+        bank_list = list(map(int, line))
+        part_1_sum += get_joltage(bank_list, PART_1_LENGTH)
+        part_2_sum += get_joltage(bank_list, PART_2_LENGTH)
+    print(part_1_sum)
+    print(part_2_sum)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+def get_joltage(bank: list[int], length: int) -> int:
+    if length == 1:
+        return max(bank)
+    largest_number = bank[0]
+    largest_idx = 0
+    for idx, num in enumerate(bank[:-(length-1)]):
+        if num > largest_number:
+            largest_number = num
+            largest_idx = idx
+    return (10**(length-1) * largest_number) + get_joltage(bank[largest_idx+1:], length-1)
 
 
 main()
