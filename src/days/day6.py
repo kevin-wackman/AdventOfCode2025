f12fd5c10d2016adb68484f8f767f3de31209170
kevin-wackman <kevin.wackman@gmail.com>
2025-12-08 14:03:11 -0600

    Add solution to part 2 of day 6

--- src/days/day6.py
+++ src/days/day6.py
@@ -9,12 +9,32 @@ MUL = '*'
 
 
 def main():
-    lines = list(map(str.strip, open_data_file_as_lines(DATA_FILE)))
-    lines = list(map(lambda x: x.split(), lines))
-    lines.reverse()
-    operators = lines[0]
-    nums = list(map(lambda x: list(map(int, x)), lines[1:]))
+    lines = list(map(lambda x : x.strip('\n'), open_data_file_as_lines(DATA_FILE)))
+    operators = lines[-1].split()
+    lines_1 = list(map(lambda x: x.split(), lines))
+    nums = list(map(lambda x: list(map(int, x)), lines_1[:-1]))
     print(solve_math_worksheet(operators, nums))
+    lines_2 = [list(row) for row in zip(*lines[:-1])]
+    print(solve_transposed_math_worksheet(operators, lines_2))
+
+def solve_transposed_math_worksheet(operators: list[str], nums) -> int:
+    sum = 0
+    problem = 0
+    problem_nums = []
+    for num_list in nums:
+        num_str = ''.join(c for c in num_list if not c.isspace())
+        if not num_str:
+            # Result is separated out for easier debugging
+            result = reduce(parse_operator(operators[problem]), problem_nums)
+            sum += result
+            problem_nums = []
+            problem += 1
+        else:
+            problem_nums.append(int(num_str))
+    # Clean up anything remaining
+    if problem_nums:
+        sum += reduce(parse_operator(operators[problem]), problem_nums)
+    return sum
 
 def parse_operator(op: str):
     if op == ADD:
